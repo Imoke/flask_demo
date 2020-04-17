1add92f8187c3b8f5379368a71d5284bf41717ce
liuwang <z003tx9r@siemens.com>
2020-04-17 10:21:24 +0000

    feat: some functions

--- package/base/app/app.py
+++ package/base/app/app.py
@@ -12,12 +12,20 @@ jobstores = {
     'default': SQLAlchemyJobStore(url='postgres://pgsql:pswd@192.168.1.61:5433/python')
 }
 executors = {
-    'default': ThreadPoolExecutor(20),
-    'processpool': ProcessPoolExecutor(5)
+    'default': ThreadPoolExecutor(5),
+    'processpool': ProcessPoolExecutor(3)
 }
 job_defaults = {
-    'coalesce': False,
+    ## 当由于某种原因导致某个job积攒了好几次没有实际运行（比如说系统挂了5分钟后恢复，有一个任务是每分钟跑一次的，
+    ## 按道理说这5分钟内本来是“计划”运行5次的，但实际没有执行），如果coalesce为True，下次这个job被submit给executor时，
+    ## 只会执行1次，也就是最后这次，如果为False，那么会执行5次（不一定，因为还有其他条件，看后面misfire_grace_time的解释）
+    'coalesce': True, 
+    ## 就是说同一个job同一时间最多有几个实例再跑，比如一个耗时10分钟的job，被指定每分钟运行1次，如果我们max_instance值为5，
+    ## 那么在第6~10分钟上，新的运行实例不会被执行，因为已经有5个实例在跑了
     'max_instances': 3
+    ## misfire_grace_time：设想和上述coalesce类似的场景，如果一个job本来14:00有一次执行，但是由于某种原因没有被调度上，
+    ## 现在14:01了，这个14:00的运行实例被提交时，会检查它预订运行的时间和当下时间的差值（这里是1分钟），大于我们设置的30秒限制，
+    ## 那么这个运行实例不会被执行。
 }
 scheduler = BackgroundScheduler(jobstores=jobstores, executors=executors, job_defaults=job_defaults, timezone=utc)
 
--- package/controller/apScheduler.py
+++ package/controller/apScheduler.py
@@ -1,10 +1,30 @@
 from package.base.app import webapp, helloBp
 from package.services.apSchedulerService import *
 
-@helloBp.route("/api/scheduler/<name>")
+@helloBp.route("/api/scheduler/job/<name>")
 def addJob(name):
     return ApScheduler.add_job(name)
 
-@helloBp.route("/api/scheduler/list")
+@helloBp.route("/api/scheduler/job/io/<name>")
+def addIoJob(name):
+    return ApScheduler.add_iojob(name)
+
+@helloBp.route("/api/scheduler/job/list")
 def jobList():
     return ApScheduler.list_jobs()
+
+@helloBp.route("/api/scheduler/job/remove/<id>")
+def removeJob(id):
+    return ApScheduler.remove_job(id)
+
+@helloBp.route("/api/scheduler/job/pause/<id>")
+def pauseJob(id):
+    return ApScheduler.pause_job(id)
+
+@helloBp.route("/api/scheduler/job/resume/<id>")
+def resumeJob(id):
+    return ApScheduler.resume_job(id)
+
+@helloBp.route("/api/scheduler/job/pause/<id>")
+def rescheduleJob(id):
+    return ApScheduler.rescheduler_job(id)
